e015d8fa411be85cd67be225f484f600bc3b5ccb
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-12-13 01:47:54 +0800

    Add unstalk functionality

---
+++ unstalk.php
@@ -0,0 +1,88 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "cruskdb";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+        if ($conn->connect_error)
+        {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        else
+        {
+            $query = "SELECT * FROM Stalkers WHERE username=? AND password=?";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param("ss", $_SESSION["username"], $_SESSION["password"]);
+            $stmt->execute();
+            $result = $stmt->fetch();
+            if ($result)
+            {
+                // echo "Hello " . $_SESSION["username"];
+                // echo '<form method="POST" action="logout.php">
+                //        <input type="submit" value="Logout">
+                //       </form>';
+            }
+            else
+            {
+                echo "You are not logged in.";
+                $stmt->close();
+                $conn->close();
+                exit();
+
+            }
+            $stmt->close();
+        }
+
+        $conn->close();
+    ?>
+
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "cruskdb";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+        if ($conn->connect_error)
+        {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        else
+        {
+            if ($stmt = $conn->prepare("DELETE FROM Stalks WHERE stalker_username=? AND crusk_student_number=?"))
+            {
+                /* bind parameters for markers */
+                $stmt->bind_param("ss", $_SESSION["username"], $_POST["student_number"]);
+                /* execute query */
+                if ($stmt->execute() === true)
+                {
+                    echo "Unstalked " . $_POST["student_number"];
+                }
+                /* close statement */
+                $stmt->close();
+            }
+        }
+
+        $conn->close();
+
+        echo '<script type="text/javascript"> window.location = "//localhost/cruskdb/home.php"</script>';
+    ?>
+</body>
+</html>
