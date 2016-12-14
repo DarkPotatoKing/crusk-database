e11b3d368c4ec51ea9e957f2a67545cff5b1e843
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-12-15 04:16:07 +0800

    Verify identity before displaying navbar

--- home.php
+++ home.php
@@ -11,48 +11,6 @@ session_start();
 
 </head>
 <body>
-
-<nav class="navbar navbar-default" role="navigation">
-  <!-- Brand and toggle get grouped for better mobile display -->
-  <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="#">Hello <?php echo $_SESSION["username"]?></a>
-  </div>
-
-  <!-- Collect the nav links, forms, and other content for toggling -->
-  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="/cruskdb/home.php">Crusks</a></li>
-      <li><a href="/cruskdb/classes.php">Classes</a></li>
-    </ul>
-
-    <div class="col-sm-3 col-md-3">
-        <form class="navbar-form" role="search" action="search.php" method="post">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search" name="search_string">
-            <div class="input-group-btn">
-                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-            </div>
-        </div>
-        </form>
-    </div>
-
-    <ul class="nav navbar-nav navbar-right">
-      <li>
-        <form action="logout.php" method="post">
-            <button type="submit" class="btn" >Log out</button>
-        </form>
-      </li>
-    </ul>
-
-  </div><!-- /.navbar-collapse -->
-</nav>
-
     <?php
         $servername = "localhost";
         $username = "root";
@@ -92,12 +50,46 @@ session_start();
         $conn->close();
     ?>
 
-    <form method="POST" action="search.php">
-        <input type="text" name="search_string" value=""><br>
-        <input type="submit" value="search">
-    </form>
+    <nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Hello <?php echo $_SESSION["username"]?></a>
+  </div>
 
-    <form method="POST" action="classes.php"><input type="submit" value="Classes"></form>
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="/cruskdb/home.php">Crusks</a></li>
+      <li><a href="/cruskdb/classes.php">Classes</a></li>
+    </ul>
+
+    <div class="col-sm-3 col-md-3">
+        <form class="navbar-form" role="search" action="search.php" method="post">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search" name="search_string">
+            <div class="input-group-btn">
+                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+            </div>
+        </div>
+        </form>
+    </div>
+
+    <ul class="nav navbar-nav navbar-right">
+      <li>
+        <form action="logout.php" method="post">
+            <button type="submit" class="btn" >Log out</button>
+        </form>
+      </li>
+    </ul>
+
+  </div><!-- /.navbar-collapse -->
+</nav>
 
 
     <h1>Crusks</h1>
