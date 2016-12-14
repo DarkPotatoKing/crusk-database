055dcbc73716392fa7ec7c6ee0e3b3bc7a9f90f7
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-12-15 04:34:00 +0800

    Make search results decent

--- search.php
+++ search.php
@@ -5,7 +5,8 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <title>Search</title>
 </head>
 <body>
     <?php
@@ -31,10 +32,6 @@ session_start();
             $result = $stmt->fetch();
             if ($result)
             {
-                echo "Hello " . $_SESSION["username"];
-                echo '<form method="POST" action="logout.php">
-                       <input type="submit" value="Logout">
-                      </form>';
 
             }
             else
@@ -51,19 +48,88 @@ session_start();
         $conn->close();
     ?>
 
-    <form method="POST" action="search.php">
-        <input type="text" name="search_string" value=""><br>
-        <input type="submit" value="search">
-    </form>
+     <nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Hello <?php echo $_SESSION["username"]?></a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+    <ul class="nav navbar-nav">
+      <li><a href="/cruskdb/home.php">Crusks</a></li>
+      <li><a href="/cruskdb/classes.php">Classes</a></li>
+    </ul>
+
+    <div class="col-sm-3 col-md-3">
+        <form class="navbar-form" role="search" action="search.php" method="post">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search" name="search_string">
+            <div class="input-group-btn">
+                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+            </div>
+        </div>
+        </form>
+    </div>
+
+    <ul class="nav navbar-nav navbar-right">
+      <li>
+        <form action="logout.php" method="post">
+            <button type="submit" class="btn" >Log out</button>
+        </form>
+      </li>
+    </ul>
+
+  </div><!-- /.navbar-collapse -->
+</nav>
+
+
 
     <h1>
         Search results
-
-        <form method="POST" action="home.php">
-            <input type="submit" value="Home">
-        </form>
-
     </h1>
+
+    <div>
+    <style scoped>
+        div {
+          margin: 20px;
+        }
+
+        ul {
+          list-style-type: none;
+          width: 500px;
+        }
+
+        h3 {
+          font: bold 20px/1.5 Helvetica, Verdana, sans-serif;
+        }
+
+        li img {
+          float: left;
+          margin: 0 15px 0 0;
+        }
+
+        li p {
+          font: 200 12px/1.5 Georgia, Times New Roman, serif;
+        }
+
+        li {
+          padding: 10px;
+          overflow: auto;
+        }
+
+        li:hover {
+          background: #eee;
+          cursor: pointer;
+        }
+
+    </style>
 
 
     <?php
@@ -93,12 +159,17 @@ session_start();
             while ($stmt->fetch())
             {
                 echo '<li>';
-                echo $student_number . "<br>";
-                echo $name . "<br>";
+                echo '<h3>'.$name.'</h3>' . "<br>";
+                echo '<b>'.$student_number . "</b><br>";
                 echo $facebook . "<br>";
                 echo $twitter . "<br>";
                 echo $instagram . "<br>";
-                printf('<form method="POST" action="stalk.php"><input type="hidden" name="student_number" value="%s"><input type="submit" value="Stalk"></form>', $student_number);
+                // printf('<form method="POST" action="stalk.php"><input type="hidden" name="student_number" value="%s"><input type="submit" value="Stalk"></form>', $student_number);
+                // <button type="submit" class="btn" >Log out</button>
+                printf('<form method="post" action="stalk.php">
+                            <input type="hidden" name="student_number" value="%s">
+                            <button type="submit" class="btn btn-primary" >Stalk</button>
+                        </form>', $student_number);
                 echo '</li><br>';
 
                 $num_rows = $num_rows + 1;
@@ -115,5 +186,6 @@ session_start();
 
         $conn->close();
     ?>
+    </div>
 </body>
 </html>
