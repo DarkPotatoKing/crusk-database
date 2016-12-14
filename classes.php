38365b715eed181091d06420177eb90348bfbdec
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-12-15 04:20:48 +0800

    Add navbar in class page

--- classes.php
+++ classes.php
@@ -5,6 +5,8 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
     <title>Home</title>
 </head>
 <body>
@@ -31,11 +33,6 @@ session_start();
             $result = $stmt->fetch();
             if ($result)
             {
-                echo "Hello " . $_SESSION["username"];
-                echo '<form method="POST" action="logout.php">
-                       <input type="submit" value="Logout">
-                      </form>';
-
             }
             else
             {
@@ -51,14 +48,46 @@ session_start();
         $conn->close();
     ?>
 
-    <form method="POST" action="search.php">
-        <input type="text" name="search_string" value=""><br>
-        <input type="submit" value="search">
-    </form>
+    <nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Hello <?php echo $_SESSION["username"]?></a>
+  </div>
 
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-    <h1>Classes</h1>
-    <form method="POST" action="home.php"><input type="submit" value="Home"></form>
+    <ul class="nav navbar-nav">
+      <li><a href="/cruskdb/home.php">Crusks</a></li>
+      <li class="active"><a href="/cruskdb/classes.php">Classes</a></li>
+    </ul>
+
+    <div class="col-sm-3 col-md-3">
+        <form class="navbar-form" role="search" action="search.php" method="post">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search" name="search_string">
+            <div class="input-group-btn">
+                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+            </div>
+        </div>
+        </form>
+    </div>
+
+    <ul class="nav navbar-nav navbar-right">
+      <li>
+        <form action="logout.php" method="post">
+            <button type="submit" class="btn" >Log out</button>
+        </form>
+      </li>
+    </ul>
+
+  </div><!-- /.navbar-collapse -->
+</nav>
 
     <?php
         $servername = "localhost";
