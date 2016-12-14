b959ee3d99b209e97f43377f06378c86b9515f26
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-12-15 02:41:14 +0800

    Add stylized login/signup page

--- index.php
+++ index.php
@@ -1,32 +1,91 @@
-<html>
-
+<!DOCTYPE html>
+<html >
 <head>
-    <title>Crusk DB</title>
+  <meta charset="UTF-8">
+  <title>Sign-Up/Login Form</title>
+  <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+
+      <link rel="stylesheet" href="css/style.css">
+
+
 </head>
 
 <body>
-    <form method="POST" action="login.php">
-        Username:<br>
-        <input type="text" name="username" value="username">
-        <br>
-        Password:<br>
-        <input type="password" name="password" value="password">
-        <br>
-        <input type="submit" value="Login">
-    </form>
+  <div class="form">
 
-    <form method="POST" action="signup.php">
-        Username:<br>
-        <input type="text" name="username" value="username"><br>
-        Password:<br>
-        <input type="password" name="password" value="password"><br>
-        Confirm password:<br>
-        <input type="password" name="confirm_password" value="password"><br>
-        <input type="submit" value="Sign up">
-    </form>
+      <ul class="tab-group">
+        <li class="tab active"><a href="#signup">Sign Up</a></li>
+        <li class="tab"><a href="#login">Log In</a></li>
+      </ul>
 
-    <?php
-        phpinfo();
-    ?>
+      <div class="tab-content">
+        <div id="signup">
+          <h1>Crusk Database</h1>
+
+          <form action="signup.php" method="post">
+
+          <div class="field-wrap">
+            <label>
+              Name<span class="req">*</span>
+            </label>
+            <input type="text"required name="username" value="" autocomplete="off"/>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Password<span class="req">*</span>
+            </label>
+            <input type="password"required name="password" value="" autocomplete="off"/>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Password confirm<span class="req">*</span>
+            </label>
+            <input type="password"required name="confirm_password" value="" autocomplete="off"/>
+          </div>
+
+          <button type="submit" class="button button-block"/>Sign Up</button>
+
+          </form>
+
+        </div>
+
+        <div id="login">
+          <h1>Crusk Database</h1>
+
+          <form action="login.php" method="post">
+
+            <div class="field-wrap">
+            <label>
+              Username<span class="req">*</span>
+            </label>
+            <input type="text" name="username" required autocomplete="off"/>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Password<span class="req">*</span>
+            </label>
+            <input type="password"required name="password" autocomplete="off"/>
+          </div>
+
+          <p class="forgot"><a href="#">Forgot Password?</a></p>
+
+          <button class="button button-block"/>Log In</button>
+
+          </form>
+
+        </div>
+
+      </div><!-- tab-content -->
+
+</div> <!-- /form -->
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+
 </body>
 </html>
