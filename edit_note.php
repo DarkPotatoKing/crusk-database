212f26b249783d5266c7d037ac77ee1ca439ff00
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-12-15 06:20:40 +0800

    Make decent

--- edit_note.php
+++ edit_note.php
@@ -5,6 +5,7 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
     <title>Home</title>
 </head>
 <body>
@@ -31,11 +32,6 @@ session_start();
             $result = $stmt->fetch();
             if ($result)
             {
-                echo "Hello " . $_SESSION["username"];
-                echo '<form method="POST" action="logout.php">
-                       <input type="submit" value="Logout">
-                      </form>';
-
             }
             else
             {
@@ -51,12 +47,92 @@ session_start();
         $conn->close();
     ?>
 
-    <form method="POST" action="edit_note_2.php">
-        <?php printf('<input type="text" name="note" value="%s"><br>', $_POST["note"]) ?>
-        <?php printf('<input type="hidden" name="student_number" value="%s"><br>', $_POST["student_number"]) ?>
-        <?php printf('<input type="hidden" name="id" value="%s"><br>', $_POST["id"]) ?>
-        <input type="submit" value="save">
-    </form>
+    <nav class="navbar navbar-default" role="navigation">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Hello <?php echo $_SESSION["username"]?></a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+        <ul class="nav navbar-nav">
+          <li><a href="/cruskdb/home.php">Crusks</a></li>
+          <li><a href="/cruskdb/classes.php">Classes</a></li>
+        </ul>
+
+        <div class="col-sm-3 col-md-3">
+            <form class="navbar-form" role="search" action="search.php" method="post">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search" name="search_string">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                </div>
+            </div>
+            </form>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <form action="logout.php" method="post">
+                <button type="submit" class="btn" >Log out</button>
+            </form>
+          </li>
+        </ul>
+
+      </div><!-- /.navbar-collapse -->
+    </nav>
+
+    <div>
+        <style scoped>
+        input[type=text], select {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        input[type=submit] {
+            width: 100%;
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type=submit]:hover {
+            background-color: #45a049;
+        }
+
+        div {
+            border-radius: 5px;
+            background-color: #f2f2f2;
+            padding: 20px;
+        }
+        </style>
+        <body>
+
+        <div>
+          <form action="edit_note_2.php" method="POST">
+            <label for="note">Class Name</label>
+            <?php printf('<input type="text" name="note" value="%s">', $_POST["note"]) ?>
+            <?php printf('<input type="hidden" name="student_number" value="%s">', $_POST["student_number"]) ?>
+            <?php printf('<input type="hidden" name="id" value="%s">', $_POST["id"]) ?>
+            <input type="submit" value="Save">
+          </form>
+        </div>
+    </div>
 
 
 </body>
