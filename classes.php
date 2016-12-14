e4258792eea925015f4c03294d37bc062bed81b6
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-12-15 05:54:11 +0800

    Replace cdm with local ref

--- classes.php
+++ classes.php
@@ -5,8 +5,7 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
     <title>Home</title>
 </head>
 <body>
