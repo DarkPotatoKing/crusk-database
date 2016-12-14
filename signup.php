7b55791ef38b790323edcf8a677d8b51f8527c1f
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-12-15 03:05:24 +0800

    Add signup style

--- signup.php
+++ signup.php
@@ -2,51 +2,80 @@
 <html>
 <head>
     <title>Sign up</title>
+  <meta charset="UTF-8">
+  <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+
+      <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
-    <?php
+  <div class="form">
 
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "cruskdb";
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        <div id="login">
+          <h1>
+            <?php
 
-        // Check connection
-        if ($conn->connect_error)
-        {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        else if ($_POST["password"] != $_POST["confirm_password"])
-        {
-            echo "Password does not match";
-        }
-        else
-        {
-            if ($stmt = $conn->prepare("INSERT INTO Stalkers (username, password) VALUES (?, ?)"))
-            {
-                /* bind parameters for markers */
-                $stmt->bind_param("ss", $_POST["username"], $_POST["password"]);
-                /* execute query */
-                if ($stmt->execute() === true)
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "cruskdb";
+
+                // Create connection
+                $conn = new mysqli($servername, $username, $password, $dbname);
+
+                // Check connection
+                if ($conn->connect_error)
                 {
-                    echo "Created user.";
+                    echo "Connection failed";
+                }
+                else if ($_POST["password"] != $_POST["confirm_password"])
+                {
+                    echo "Passwords do not match";
                 }
                 else
                 {
-                    echo "'".  $_POST["username"] . "'" . " is already taken.";
+                    if ($stmt = $conn->prepare("INSERT INTO Stalkers (username, password) VALUES (?, ?)"))
+                    {
+                        /* bind parameters for markers */
+                        $stmt->bind_param("ss", $_POST["username"], $_POST["password"]);
+                        /* execute query */
+                        if ($stmt->execute() === true)
+                        {
+                            echo "Sign Up succesful";
+                        }
+                        else
+                        {
+                            echo "'".  $_POST["username"] . "'" . " is already taken";
+                        }
+                        /* close statement */
+                        $stmt->close();
+                    }
                 }
-                /* close statement */
-                $stmt->close();
-            }
-        }
 
-        $conn->close();
+                $conn->close();
 
-    ?>
+            ?>
+          </h1>
+
+          <form action="index.php" method="post">
+
+          <button class="button button-block"/>Log In/Sign Up</button>
+
+          </form>
+
+        </div>
+
+
+
+</div> <!-- /form -->
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+
+
 
 </body>
 </html>
