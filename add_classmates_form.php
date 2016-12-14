0a65b119d455e48376706ad492a8e0fbf706aab1
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-12-15 06:50:40 +0800

    Make add classmate form decent

--- add_classmates_form.php
+++ add_classmates_form.php
@@ -5,6 +5,7 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
     <title>Home</title>
 </head>
 <body>
@@ -36,11 +37,6 @@ session_start();
             $result = $stmt->fetch();
             if ($result)
             {
-                echo "Hello " . $_SESSION["username"];
-                echo '<form method="POST" action="logout.php">
-                       <input type="submit" value="Logout">
-                      </form>';
-
             }
             else
             {
@@ -56,18 +52,87 @@ session_start();
         $conn->close();
     ?>
 
-    <form method="POST" action="home.php"><input type="submit" value="Home"></form>
+    <nav class="navbar navbar-default" role="navigation">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Hello <?php echo $_SESSION["username"]?></a>
+      </div>
 
-    <form action="add_classmates.php" method="post">
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-    <?php echo $_POST['class_name'] ?>
-    <br />
-    <!-- <input type="checkbox" name="formDoor[]" value="A" />Acorn Building<br />
-    <input type="checkbox" name="formDoor[]" value="B" />Brown Hall<br />
-    <input type="checkbox" name="formDoor[]" value="C" />Carnegie Complex<br />
-    <input type="checkbox" name="formDoor[]" value="D" />Drake Commons<br />
-    <input type="checkbox" name="formDoor[]" value="E" />Elliot House<br /> -->
+        <ul class="nav navbar-nav">
+          <li><a href="/cruskdb/home.php">Crusks</a></li>
+          <li><a href="/cruskdb/classes.php">Classes</a></li>
+        </ul>
 
+        <div class="col-sm-3 col-md-3">
+            <form class="navbar-form" role="search" action="search.php" method="post">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search" name="search_string">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                </div>
+            </div>
+            </form>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <form action="logout.php" method="post">
+                <button type="submit" class="btn" >Log out</button>
+            </form>
+          </li>
+        </ul>
+
+      </div><!-- /.navbar-collapse -->
+    </nav>
+
+    <div>
+        <style scoped>
+        input[type=text], select {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        input[type=submit] {
+            width: 100%;
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type=submit]:hover {
+            background-color: #45a049;
+        }
+
+        div {
+            border-radius: 5px;
+            background-color: #f2f2f2;
+            padding: 20px;
+        }
+        </style>
+        <body>
+
+        <div>
+          <form action="add_classmates.php" method="POST">
+            <label for="classmates">Classmates on <?php echo $_POST['class_name'] ?></label>
+
+    <br>
     <?php
         $servername = "localhost";
         $username = "root";
@@ -115,7 +180,7 @@ session_start();
                 $stmt->execute();
                 $stmt->bind_result($name);
                 $stmt->fetch();
-                printf('<input type="checkbox" name="stalk_id[]" value="%s" />%s %s<br />',
+                printf('<input type="checkbox" name="stalk_id[]" value="%s" /> %s %s<br />',
                         $stalk_ids[$i], $student_numbers[$i], $name);
                 $stmt->close();
             }
@@ -126,9 +191,10 @@ session_start();
     ?>
 
 
-    <input type="submit" name="formSubmit" value="Submit" />
-
-    </form>
+            <input type="submit" value="Save">
+          </form>
+        </div>
+    </div>
 
 
 </body>
